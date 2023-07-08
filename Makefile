ecc437d1d3e783f85416bdc97808e28d5ee46df6
umizu <ninjaz.gg@outlook.com>
2023-07-08 13:26:03 -0400

    cmt

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 build:
 	@go build -o bin/gohon
 
-run:
+run: build
 	@./bin/gohon
 
 test:
---
+++ api.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleBook))
+}
+
+func (s *APIServer) handleBook(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleGetBook(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleCreateBook(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleDeleteBook(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
